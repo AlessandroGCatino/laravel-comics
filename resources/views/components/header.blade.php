fe81a0a0321a03a9dfbad51e65c4e4caeec2c821
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-03-13 16:41:49 +0100

    import immagini e creazione file

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,20 @@
 <h1>Componente Header</h1>
+
+<div id="external">
+    <div class="container py-3 d-flex justify-content-between">
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="Header Logo" width="70">
+        </figure>
+        <nav class="d-flex align-items-center">
+            <ul class="d-flex gap-3 text-uppercase">
+                <li v-for= "(elements, index) in navElements" :key="index">
+                    <a :href="elements.link"> {{ elements.name }} </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+</div>
 <nav>
     <ul>
         <li><a href="{{ route('homePage') }}">Home</a></li>
